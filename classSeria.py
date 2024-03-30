09db6e512607e6570d3263345fa4bc74d797af9d
maroslaw140 <marek.muller911@gmail.com>
2024-03-30 22:00:22 +0100

    dodano obsługę błędu w klasie Seria

--- classSeria.py
+++ classSeria.py
@@ -7,17 +7,27 @@ class Seria:
         self.serie = []
 
     def pobierz_kategorie(self):
-        self.kursor.execute("SELECT nazwa FROM seria")
-        self.serie = self.kursor.fetchall()
+        try:
+            self.kursor.execute("SELECT nazwa FROM seria")
+            self.serie = self.kursor.fetchall()
+        except cx_Oracle.Error as error:
+            print(error)
+            funkcje.zapisz_blad(error)
 
     def generuj_dane(self, liczba_danych=1):
-        for _ in range(liczba_danych):
-            nazwa_serii = funkcje.losowy_ciag(255)
-
-            while nazwa_serii in self.serie:
+        try:
+            for _ in range(liczba_danych):
                 nazwa_serii = funkcje.losowy_ciag(255)
 
-            self.serie.append(nazwa_serii)
-            self.kursor.execute("""INSERT INTO seria (nazwa) VALUES(:nazwa)""", {'nazwa': nazwa_serii})
+                while nazwa_serii in self.serie:
+                    nazwa_serii = funkcje.losowy_ciag(255)
 
+                self.serie.append(nazwa_serii)
+                self.kursor.execute("""INSERT INTO seria (nazwa) VALUES(:nazwa)""", {'nazwa': nazwa_serii})
 
+            self.kursor.connection.commit()
+
+        except cx_Oracle.Error as error:
+            print(error)
+            funkcje.zapisz_blad(error)
+            self.kursor.connection.rollback()
