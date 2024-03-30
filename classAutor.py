25eef8b3d9efe7f0794190f46fb752eb27ac0172
maroslaw140 <marek.muller911@gmail.com>
2024-03-30 21:57:39 +0100

    dodano klasę Autor

---
+++ classAutor.py
@@ -0,0 +1,17 @@
+import cx_Oracle
+import funkcje
+
+class Autor:
+    def __init__(self, kursor):
+        self.kursor = kursor
+
+    def generuj_dane(self, liczba_danych=1):
+        try:
+            for _ in range(liczba_danych):
+                self.kursor.execute("""INSERT INTO autor (imie, nazwisko) VALUES (:imie, :nazwisko)""", {'imie': funkcje.losowe_imie(), 'nazwisko': funkcje.losowe_nazwisko()})
+
+            self.kursor.connection.commit()
+        except cx_Oracle.Error as error:
+            print(error)
+            funkcje.zapisz_blad(error)
+            self.kursor.connection.rollback()
